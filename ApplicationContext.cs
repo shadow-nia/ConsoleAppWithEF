d017e6472c55a16d7db91c6840bdd23879187107
shadow-nia <danyil.dykyi@gmail.com>
2025-07-01 15:36:32 +0200

    initial files

---
+++ ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleAppWithEF;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=consoleappwithef.db");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleAppWithEF;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    //// Create our objects
+    //User alex = new User { Name = "Alex", Age = 24 };
+    //User magda = new User { Name = "Magda", Age = 30 };
+
+    //// Add our objects to database
+    //db.Users.AddRange(alex, magda);
+    //db.SaveChanges();
+    //Console.WriteLine("The objects have been added successfully!");
+
+    // Read out objects from database
+    var users = db.Users.ToList();
+    Console.WriteLine("List of objects:");
+    foreach (var user in users) Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+   
+}
+
+
