cc7bab7ee7cec44f689868bfd59e01d11e77bd8a
shadow-nia <danyil.dykyi@gmail.com>
2025-07-01 16:24:26 +0200

    CRUD base using

--- Program.cs
+++ Program.cs
@@ -2,20 +2,55 @@
 
 using (ApplicationContext db = new ApplicationContext())
 {
-    //// Create our objects
-    //User alex = new User { Name = "Alex", Age = 24 };
-    //User magda = new User { Name = "Magda", Age = 30 };
+    // Creating our object
+    User jacob = new User { Name = "Jacob", Age = 32 };
+    // Adding our object to database 
+    db.Users.Add(jacob);
+    db.SaveChanges();
+    Console.WriteLine("The object has been added successfully!");
 
-    //// Add our objects to database
-    //db.Users.AddRange(alex, magda);
-    //db.SaveChanges();
-    //Console.WriteLine("The objects have been added successfully!");
+}
 
-    // Read out objects from database
+using (ApplicationContext db = new ApplicationContext())
+{
+    // Getting our objects from database
     var users = db.Users.ToList();
     Console.WriteLine("List of objects:");
     foreach (var user in users) Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
-   
 }
 
+using (ApplicationContext db = new ApplicationContext())
+{
+    // Updating last object from database
+    User? user = db.Users.OrderBy(u => u.Id).LastOrDefault();
+    if (user != null)
+    {
+        user.Name = "Thomas";
+        user.Age = 28;
+        //db.Users.Update(user);
+        db.SaveChanges();
+    }
 
+    // Checking our new database
+    var users = db.Users.ToList();
+    Console.WriteLine("List of objects after updating:");
+    foreach (var u in users) Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    // Deleting first object from database
+    User? user = db.Users.OrderBy(u => u.Id).LastOrDefault();
+    if (user != null)
+    {
+        db.Users.Remove(user);
+        db.SaveChanges();
+    }
+
+    // Checking our new database
+    var users = db.Users.ToList();
+    Console.WriteLine("List of objects after deleting:");
+    foreach (var u in users) Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+
+}
